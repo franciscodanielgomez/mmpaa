48cd311841c659c670e0134b8a860117b30b3357
Francisco Daniel Gomez <107729864+franciscodanielgomez@users.noreply.github.com>
2024-10-12 22:54:02 -0300

    Update: Carrousel

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -1,30 +1,51 @@
+"use client";
+
 import Link from "next/link";
 import Container from "@/components/container";
 import PostList from "@/components/postlist";
+import { useState, useEffect } from "react";
 
-const HeaderBanner = () => {
+// Componente del Carrousel para el Header
+export const HeaderCarousel = () => {
+  const images = [
+    '/img/hero1.png', // Asegúrate de que las imágenes estén en la carpeta public
+    '/img/hero2.png',
+    '/img/hero3.png'
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Cambia cada 3 segundos
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
-    <div
-      className="mb-10 flex flex-col items-start justify-center h-96 text-left p-5 rounded-md overflow-hidden relative"
-      style={{
-        backgroundImage: "url('https://www.foronuclear.org/wp-content/uploads/2010/06/atomo-y-electrones-854x465.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "left"
-      }}
-    >
-      <div className="absolute inset-0 bg-black bg-opacity-70 rounded-md"></div>
-      <div className="relative z-10 p-5">
-        <h2 className="text-2xl font-bold text-white mb-4">Welcome to the MMPAA</h2>
-        <p className="text-gray-300 mb-6">
-          Join us in our mission to advance the field of medical physics.
-        </p>
-        <Link
-          href="/archive"
-          className="inline-flex items-center gap-1 rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
-        >
-          Learn More
-        </Link>
-      </div>
+    <div className="relative w-full h-96 overflow-hidden rounded-md">
+      <img
+        src={images[currentImageIndex]}
+        alt={`Hero ${currentImageIndex}`}
+        className="w-full h-96 object-cover transition duration-500 ease-in-out rounded-md"
+      />
+
+
     </div>
   );
 };
@@ -36,7 +57,7 @@ const Banner = ({ title, description, imageUrl }) => {
       style={{
         backgroundImage: `url('${imageUrl}')`,
         backgroundSize: "cover",
-        backgroundPosition: "left"
+        backgroundPosition: "left",
       }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-70 rounded-md"></div>
@@ -56,21 +77,23 @@ const Banner = ({ title, description, imageUrl }) => {
 
 const AboutUsBanner = () => {
   return (
-    <div className="mb-10"> {/* Usamos un margen inferior más grande */}
+    <div className="mb-10">
       <div
         className="flex flex-col items-start justify-center h-64 text-left p-5 rounded-md overflow-hidden relative"
         style={{
-          backgroundImage: "url('https://www.foronuclear.org/wp-content/uploads/2010/06/atomo-y-electrones-854x465.jpg')",
+          backgroundImage:
+            "url('https://www.foronuclear.org/wp-content/uploads/2010/06/atomo-y-electrones-854x465.jpg')",
           backgroundSize: "cover",
-          backgroundPosition: "left"
+          backgroundPosition: "left",
         }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-70 rounded-md"></div>
         <div className="relative z-10 p-5">
           <h3 className="text-xl font-bold text-white mb-2">About Us</h3>
           <p className="text-gray-300 mb-4">
-            The Medical Physics Alumni Association is a community of professionals dedicated to advancing medical physics
-            through collaboration and education.
+            The Medical Physics Alumni Association is a community of
+            professionals dedicated to advancing medical physics through
+            collaboration and education.
           </p>
           <Link
             href="/about"
@@ -84,40 +107,37 @@ const AboutUsBanner = () => {
   );
 };
 
-
-
+// Mantén la exportación por defecto de la función Post
 export default function Post({ posts }) {
   return (
     <>
       {posts && (
         <Container>
-          {/* Agregamos el HeaderBanner aquí */}
-          <HeaderBanner />
+          {/* Usamos el HeaderCarousel aquí */}
+          <HeaderCarousel />
 
           {/* Nuevos Banners en Dos Columnas */}
           <div className="mt-10 grid gap-10 md:grid-cols-2">
             <Banner
               title="Do You Want to Be a Member?"
               description="Being an active member of the MMPAA involves proactive engagement and contribution to the collective pursuit of knowledge."
-              imageUrl="https://example.com/membership-banner.jpg" // Puedes cambiar esta URL a la imagen que desees
+              imageUrl="https://example.com/membership-banner.jpg"
             />
             <Banner
               title="Active Members"
               description="An active member catalyzes the association’s mission, driving innovation, and propelling scientific inquiry forward through sustained dedication and involvement."
-              imageUrl="https://example.com/active-members-banner.jpg" // Cambia esto por la imagen que desees
+              imageUrl="https://example.com/active-members-banner.jpg"
             />
           </div>
 
-          {/* Agregamos el About Us Banner */}
+          {/* About Us Banner */}
           <div className="mt-10">
             <AboutUsBanner />
           </div>
 
-
-
-
-          <div className="grid gap-10 md:grid-cols-2 lg:gap-10 ">
-            {posts.slice(0, 2).map(post => (
+          {/* Lista de posts */}
+          <div className="grid gap-10 md:grid-cols-2 lg:gap-10">
+            {posts.slice(0, 2).map((post) => (
               <PostList
                 key={post._id}
                 post={post}
@@ -127,13 +147,13 @@ export default function Post({ posts }) {
             ))}
           </div>
 
-          <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
-            {posts.slice(2, 14).map(post => (
+          <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3">
+            {posts.slice(2, 14).map((post) => (
               <PostList key={post._id} post={post} aspect="square" />
             ))}
           </div>
 
-
+          {/* Link al archivo */}
           <div className="mt-10 flex justify-center">
             <Link
               href="/archive"
@@ -142,8 +162,6 @@ export default function Post({ posts }) {
               <span>View all Announcements</span>
             </Link>
           </div>
-
-
         </Container>
       )}
     </>
